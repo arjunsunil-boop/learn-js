c131f38be67aa128e59e81446c13a7e05c693f60
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-05-31 00:00:19 +0530

    checked property

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-//use Math module to use various math functions 
+const checkbox  = document.getElementById("chkbx")
+const myRadio = document.getElementById("myRadio")
+const myRadio2 = document.getElementById("myRadio2")
+const para1 = document.getElementById("para1")
+const para2 = document.getElementById("para2")
+const mySubmit = document.getElementById("mySubmit")
 
-const min = 0
-const max = 100
 
-const randombtn = document.getElementById("reset")
+mySubmit.onclick = function(){
+ 
+    if(checkbox.checked){
+        para1.textContent = `you are checked`
+        if (myRadio.checked) {
+            para2.textContent = `you selected card1`
+        }
+        else if(myRadio2.checked){
 
-const randomId = document.getElementById("random")
+             para2.textContent = `you selected card2`
 
-randombtn.onclick = function(){
-    let randomNum = Math.round(Math.random() * (max - min)) +min
-    randomId.textContent = randomNum
+
+        }
+        else {
+
+           window.alert("you havent selected any card")
+
+        }
+
+    }
+    else{
+        para1.textContent = `you are not checked`
+    }
+
 }
