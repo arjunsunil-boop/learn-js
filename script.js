211ebc2fa4870d9589ae2f8384e70384d84d54d7
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-05-30 23:33:11 +0530

    random num

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //use Math module to use various math functions 
+
+const min = 0
+const max = 100
+
+const randombtn = document.getElementById("reset")
+
+const randomId = document.getElementById("random")
+
+randombtn.onclick = function(){
+    let randomNum = Math.round(Math.random() * (max - min)) +min
+    randomId.textContent = randomNum
+}
