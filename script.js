a58a5cd1f3e8cb149996da20830916e07eb031ca
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2025-05-31 10:30:19 +0530

    random num

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-const checkbox  = document.getElementById("chkbx")
-const myRadio = document.getElementById("myRadio")
-const myRadio2 = document.getElementById("myRadio2")
+const num = document.getElementById("num")
+
 const para1 = document.getElementById("para1")
-const para2 = document.getElementById("para2")
-const mySubmit = document.getElementById("mySubmit")
 
+const submit = document.getElementById("submit")
 
-mySubmit.onclick = function(){
- 
-    if(checkbox.checked){
-        para1.textContent = `you are checked`
-        if (myRadio.checked) {
-            para2.textContent = `you selected card1`
-        }
-        else if(myRadio2.checked){
+const min = 1
+const max =6
 
-             para2.textContent = `you selected card2`
-
-
-        }
-        else {
-
-           window.alert("you havent selected any card")
-
-        }
-
+submit.onclick =  function(){
+    n = Number(num.value)
+    let random = Math.round(Math.random() *(max-min+1)) + min
+    console.log(random)
+    if (n===random) {
+        para1.textContent = `You guessed it right`
+    } else {
+        para1.textContent = `Better luck next time`
     }
-    else{
-        para1.textContent = `you are not checked`
-    }
-
 }
